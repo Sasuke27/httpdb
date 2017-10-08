22205c0588629b3f4ec63e1e8b1b8d9d9b66e19a
Khent Arjiel Cabural <khentarjielcabural@gmail.com>
2017-10-08 20:39:00 +0800

    Signed-off-by: Khent Arjiel Cabural <khentarjielcabural@gmail.com>

---
+++ src/app/removeperson/removeperson.component.ts
@@ -0,0 +1,50 @@
+
+import { Component, OnInit } from '@angular/core';
+import { DbService } from '../services/db.service';
+import { NgForm } from '@angular/forms';
+import { IPerson } from '../interfaces/iperson';
+
+@Component({
+  selector: 'app-removeperson',
+  templateUrl: './removeperson.component.html',
+  styleUrls: ['./removeperson.component.css']
+})
+export class RemovepersonComponent implements OnInit {
+  baseURL = 'https://first-pro-5cc9a.firebaseio.com';
+  rootNode = 'people';
+  refID: any;
+
+  fname: string;
+  lname: string;
+
+  person: object;
+
+  constructor(private dbService: DbService) { }
+
+  ngOnInit() {
+  }
+
+  editData(id){
+     console.log(id);
+     this.refID = id;
+     this.dbService.getID(`${this.baseURL}/${this.rootNode}/${this.refID}.json`)
+     .subscribe(
+       (response) => {
+           this.fname = response.firstName;
+           this.lname = response.lastName;
+       },
+       (error) => console.log(error)
+     );
+  }
+
+  deletingData(id) {
+
+       this.dbService.deleteData(`${this.baseURL}/${this.rootNode}/${this.refID}.json`,this.person)
+       .subscribe(
+         (response) => console.log(response),
+         (error) => console.log(error)
+       );
+  }
+
+}
+
